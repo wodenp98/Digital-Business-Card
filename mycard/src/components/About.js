459fee310827846aac373b3c7f0242af51edc01a
Paul VIGNERON <paulvigneron4698@gmail.Com>
2022-03-31 20:09:43 +0200

    update css

--- mycard/src/components/About.js
+++ mycard/src/components/About.js
@@ -4,11 +4,11 @@ function About() {
     return (
         <div className="profile">
         <div className="about">
-            <h2>About</h2>
+            <h2>À propos</h2>
             <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
         </div>
         <div className="interests">
-            <h2>Interests</h2>
+            <h2>Hobbies</h2>
             <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
         </div>
         </div>
